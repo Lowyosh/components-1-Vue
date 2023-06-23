1bb5fa55fa9276fdf004acb67d052b7181f5503b
Yousi <josealzdz@gmail.com>
2023-06-23 17:45:15 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import BlogPost from './components/BlogPost.vue';
+import ButtonCounter from './components/ButtonCounter.vue';
+import { ref } from 'vue';
+
+
+const posts = ref([
+  {title: 'Post 1', id: 1, body: 'Descripción 1'},
+  {title: 'Post 2', id: 2, body: 'Descripción 2'},
+  {title: 'Post 3', id: 3 },
+]);
+
+const favorito = ref('')
+const cambiarFavorito = (title) => {
+  favorito.value = title;
+}
+
+</script>
+
+
+
+<template>
+  <div class="container">
+    <h1>App</h1>
+    <h2>Mis post favorito: {{ favorito }} </h2>
+  </div>
+  <BlogPost
+  v-for="post in posts"
+  :key="post.id"
+  :title="post.title"
+  :id="post.id"
+  :body="post.body"
+  @cambiarFavoritoNombre = 'cambiarFavorito'
+  />
+
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,29 @@
+<script setup>
+// defineProps(['title', 'id', 'body']);
+defineProps({
+    title: String,
+    id: Number,
+    body: {
+        type: String,
+        default: 'Sin Descripción',
+    }
+})
+const emit = defineEmits(['cambiarFavoritoNombre'])
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{title}}</h5>
+            <p>{{body}}</p>
+            <button @click="emit('cambiarFavoritoNombre', title)" class="btn btn-outline-primary">Fav</button>
+        </div>
+    </div>
+</template>
+
+
+
+<style>
+
+</style>
